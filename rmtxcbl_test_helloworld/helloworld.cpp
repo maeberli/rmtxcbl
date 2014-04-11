b712ce45764510fd083df7a5da974ec6df499acf
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-04-11 09:41:11 +0200

    ADD: Implementation of test hello wolrd project.

---
+++ rmtxcbl_test_helloworld/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void helloworldFunction();
+
+int main()
+{
+    helloworldFunction();
+    return 0;
+}
+
+void helloworldFunction()
+{
+    std::cout << "Hello world" << std::endl;
+}
