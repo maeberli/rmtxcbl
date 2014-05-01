915d1eeb49fac04d232f0b1ae69ccd91e0fa9e36
Marco Aeberli <marco.aeberli@he-arc.ch>
2014-05-01 22:43:11 +0200

    ADD: additional argument to specify the label

--- rmtxcbl_cnsl/rmtxbl_cnsl.cpp
+++ rmtxcbl_cnsl/rmtxbl_cnsl.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include "console.hpp"
 
-bool parseArgs(int argc, char** argv, std::string *out_exec, std::string *out_remhost);
+bool parseArgs(int argc, char** argv,
+        std::string *out_exec, std::string *out_label, std::string *out_remhost);
 void printHelp(char *argv0, std::string errorMsg);
 void printHelp(char *argv0);
 void printError(std::string message);
@@ -13,14 +14,14 @@ int main(int argc, char** argv)
 	
     std::string exec = "";
     std::string remhost = "";
-    if( !parseArgs(argc, argv, &exec, &remhost) )
+    std::string label = "";
+    if( !parseArgs(argc, argv, &exec, &label, &remhost) )
     {
         printHelp(argv[0], "Invalid arguments");
         return 2;
     }
     
     const int port = 6686;
-    const std::string label = "helloWorld";
     std::cout << std::endl << "===================================" << std::endl;
     std::cout << "Exec to send\t: " << exec << std::endl;
     std::cout << "With label\t: " << label << std::endl;
@@ -34,13 +35,15 @@ int main(int argc, char** argv)
     return 0;
 }
 
-bool parseArgs(int argc, char** argv, std::string *out_exec, std::string *out_remhost)
+bool parseArgs(int argc, char** argv,
+        std::string *out_exec, std::string *out_label, std::string *out_remhost)
 {
-    if(argc < 3)
+    if(argc < 4)
         return false;
 
     *out_exec = std::string( argv[1] );
-    *out_remhost = std::string( argv[2] );
+    *out_label = std::string( argv[2] );
+    *out_remhost = std::string( argv[3] );
 
     
     return true;
@@ -58,10 +61,11 @@ void printHelp(char *argv0)
               << " This application sends an excutable to remote rmtxcbl_runner" << std::endl
               << " and launches it on the remote host." << std::endl
               << std::endl
-              << "usage: " << argv0 << " exec remhost" << std::endl
+              << "usage: " << argv0 << " exec label remhost" << std::endl
               << std::endl
               << "  exec     path to the local executable, " << std::endl
               << "            which will be transfered and executed" << std::endl
+              << "  label    label which is associated to the execution" << std::endl
               << "  remhost  destination host or ip-address containing" << std::endl
               << "            the rmtxcbl_rnnr" << std::endl;
 }
